7ed5a10ad6ef7ef1510dcb1cb0b3cbf5b967be17
DmitryGorbi <godmni@gmail.com>
2023-02-26 21:34:09 +0400

    заканчиваю с каталогом

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 const navSwitch = document.querySelector('.button-switch');
 const mainNav = document.querySelector('.main-nav');
+const lineMenu = document.querySelector('.page-header__content');
 
 navSwitch.classList.remove('button-switch--nojs');
 mainNav.classList.remove('main-nav--nojs');
@@ -11,10 +12,12 @@ navSwitch.addEventListener('click', function() {
     mainNav.classList.remove('main-nav--closed');
     navSwitch.classList.add('button-switch--open');
     mainNav.classList.add('main-nav--open');
+    lineMenu.classList.remove('page-header__content--line');
   } else {
     navSwitch.classList.add('button-switch--closed');
     mainNav.classList.add('main-nav--closed');
     navSwitch.classList.remove('button-switch--open');
     mainNav.classList.remove('main-nav--open');
+    lineMenu.classList.add('page-header__content--line');
   };
 })
